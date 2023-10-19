ad7b6647dd2c13fbce2871806e15d54260f04f18
ybs <ybsetiyawan@gmail.com>
2023-10-19 14:28:05 +0700

    update css & create blogspot.jsx

---
+++ src/component/Post/Post.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Post = (props) => {
+    return (
+        <div className="post">
+                    <div className="img-thumb">
+                        <img src="https://cdn.media.amplience.net/i/petsathome/mb-cat_1?w=400&" alt=""/>
+                    </div>
+                    <div className="content">
+                        <p className="title">Dummy Title</p>
+                        <p className="desc">Dummy Body Here</p>
+                    </div>
+                </div>
+    )
+}
+
+export default Post;
