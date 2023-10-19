6876398e46daf5466b063c477eae9ffa86b54e6f
ybs <ybsetiyawan@gmail.com>
2023-10-19 11:20:09 +0700

    update css

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,22 +1,22 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 import './BlogPost.css';
 
 
 class BlogPost extends Component {
     render() {
         return(
-            <div>
+            <Fragment>
                 <p>Blog Post</p>
                 <div className="post">
                     <div className="img-thumb">
-                        <img src="" alt=""/>
+                        <img src="https://placekitten.com/640/360" alt=""/>
                     </div>
                     <div className="content">
                         <p className="title">Dummy Title</p>
                         <p className="desc">Dummy Body Here</p>
                     </div>
                 </div>
-            </div>
+            </Fragment>
         )
     }
 }
