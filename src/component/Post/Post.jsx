96bbfb72e7638765e30985c222029fcce38d0e1f
ybs <ybsetiyawan@gmail.com>
2023-10-20 08:28:21 +0700

    update post via fake api

--- src/component/Post/Post.jsx
+++ src/component/Post/Post.jsx
@@ -7,8 +7,8 @@ const Post = (props) => {
                         <img src="https://cdn.media.amplience.net/i/petsathome/mb-cat_1?w=400&" alt=""/>
                     </div>
                     <div className="content">
-                        <p className="title">Dummy Title</p>
-                        <p className="desc">Dummy Body Here</p>
+                        <p className="title">{props.title}</p>
+                        <p className="desc">{props.desc}</p>
                     </div>
                 </div>
     )
--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,14 +1,51 @@
 import React, {Component, Fragment} from "react";
 import './BlogPost.css';
 import Post from "../../component/Post/Post";
+import axios from "axios";
 
 
 class BlogPost extends Component {
+
+    state = {
+        post: []
+    }
+
+    componentDidMount(){
+
+        // cara pemanggilan API dengan menggunakan fetch
+
+        // ketika komponen berhasil di mount , kita akan memanggil api 
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        // // respons dari url dirubah ke json
+        // .then(response => response.json())
+        // // memunculkan json ke console
+        // // .then(json => console.log(json))
+        //     .then(json => {
+        //         this.setState({
+        //             post: json
+        //         })
+        //     })
+
+        // cara pemanggilan menggunakan axios
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((result) => {
+            // console.log(result.data)
+            this.setState({
+                post: result.data
+            })
+        })
+
+    }
+
     render() {
         return(
             <Fragment>
                 <p className="section-title">Blog Post</p>
-                <Post title="title" desc="desc"/>
+                {
+                    this.state.post.map(post => {
+                        return  <Post key={post.id} title={post.title} desc={post.body}/>
+                    })
+                }
             </Fragment>
         )
     }
